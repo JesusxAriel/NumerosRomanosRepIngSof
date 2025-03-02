0c9a9aaf5a42738832878a6689f61a82f783f1e1
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-03-02 21:59:18 +0100

    Presenter y index modificados y ejecutados perfectamente con 'npm start'

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,22 @@
-import sumar from "./sumador";
+import { convertirARomano } from "./numeros_romanos.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const numeroInput = document.querySelector("#numero");
+const form = document.querySelector("#convertir-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const numero = Number.parseInt(numeroInput.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  if (isNaN(numero) || numero <= 0 || numero > 3999) {
+    div.innerHTML = "<p>Por favor, introduce un número válido entre 1 y 3999.</p>";
+  } else {
+    try {
+      const romano = convertirARomano(numero);
+      div.innerHTML = "<p>El número " + numero + " en romano es: " + romano + "</p>";
+    } catch (error) {
+      div.innerHTML = "<p>Ocurrió un error al convertir el número.</p>";
+    }
+  }
 });
